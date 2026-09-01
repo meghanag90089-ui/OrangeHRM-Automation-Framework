4598bdee4ccf4591738a80b130438baef9409224
meghanagowdaa03 <meghanagowda432@gmail.com>
2026-09-01 20:50:55 +0530

    Add Extent Reports and update screenshots

--- src/test/java/com/orangehrm/utils/ScreenshotUtil.java
+++ src/test/java/com/orangehrm/utils/ScreenshotUtil.java
@@ -1,6 +1,6 @@
 package com.orangehrm.utils;
 
-import java.io.File;
+/*import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
@@ -12,10 +12,10 @@ import org.openqa.selenium.WebDriver;
 
 public class ScreenshotUtil {
 	
-	public static void captureScreenshot(WebDriver driver, String testName) {
+	public static String captureScreenshot(WebDriver driver, String testName) {
 
-	    TakesScreenshot ts = (TakesScreenshot) driver;
-	    File source = ts.getScreenshotAs(OutputType.FILE);
+	    //TakesScreenshot ts = (TakesScreenshot) driver;
+	    //File source = ts.getScreenshotAs(OutputType.FILE);
 	    
 	    //File screenshotDir = new File("screenshots");
 	    File screenshotDir = new File("target/screenshots");
@@ -27,19 +27,61 @@ public class ScreenshotUtil {
 	    /*File destination = new File(
 	        "screenshots/" + testName + ".png"
 	    );*/
-	    File destination = new File(
+	    /*File destination = new File(
 	    	    "target/screenshots/" + testName + ".png"
 	    	);
 
 	    try {
-	    	Files.copy(
-	    		    source.toPath(),
-	    		    destination.toPath(),
-	    		    StandardCopyOption.REPLACE_EXISTING
-	    		);
+	    	FileUtils.copyFile(source, destination);
+	        
+	        return destination.getAbsolutePath();
+	        
 	    } catch (IOException e) {
 	        e.printStackTrace();
+	        return null;
 	    }
 	}
 
+}*/
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class ScreenshotUtil {
+
+    public static String captureScreenshot(WebDriver driver, String testName) {
+
+        TakesScreenshot ts = (TakesScreenshot) driver;
+
+        File source = ts.getScreenshotAs(OutputType.FILE);
+
+        File screenshotDir = new File("target/screenshots");
+
+        if (!screenshotDir.exists()) {
+            screenshotDir.mkdirs();
+        }
+
+        File destination = new File(
+                "target/screenshots/" + testName + ".png"
+        );
+
+        try {
+            Files.copy(
+                    source.toPath(),
+                    destination.toPath(),
+                    StandardCopyOption.REPLACE_EXISTING
+            );
+
+            return destination.getAbsolutePath();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
--- src/test/java/com/orangehrm/utils/TestListener.java
+++ src/test/java/com/orangehrm/utils/TestListener.java
@@ -1,6 +1,6 @@
 package com.orangehrm.utils;
 
-import org.testng.ITestResult;
+/*import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 
 import com.orangehrm.base.BaseTest;
@@ -18,6 +18,70 @@ public class TestListener extends TestListenerAdapter {
 	            result.getName()
 	        );
 	    }
-	}
+	}*/
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.orangehrm.base.BaseTest;
+
+public class TestListener extends TestListenerAdapter {
+
+    static ExtentReports extent;
+    static ExtentTest test;
+
+    static {
+        ExtentSparkReporter spark =
+                new ExtentSparkReporter("target/ExtentReports/ExtentReport.html");
+
+        extent = new ExtentReports();
+        extent.attachReporter(spark);
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+
+        test = extent.createTest(result.getName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+
+        test.pass("Test Passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+
+        test.fail(result.getThrowable());
+
+        Object testClass = result.getInstance();
+        BaseTest baseTest = (BaseTest) testClass;
+
+        String screenshotPath =
+                ScreenshotUtil.captureScreenshot(
+                        baseTest.getDriver(),
+                        result.getName()
+                );
+
+        if (screenshotPath != null) {
+            test.addScreenCaptureFromPath(screenshotPath);
+        }
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+
+        test.skip("Test Skipped");
+    }
+
+    @Override
+    public void onFinish(org.testng.ITestContext context) {
+
+        extent.flush();
+    }
+}
 
 
